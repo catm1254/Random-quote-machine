e299882fdd848b64aec160ac3e06c26d1b9e44f3
Carlos_Torres <carlos.a.torres.mtz@gmail.com>
2024-09-18 14:46:40 -0600

    animation Text

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,11 @@ interface Quote {
   quote: string;
   author: string;
 }
+
 const getRandomQuote = (): Quote => {
   return quotes[Math.floor(Math.random() * quotes.length)];
 };
+
 const getRandomColor = (): string => {
   const red = Math.floor(Math.random() * 128);
   const green = Math.floor(Math.random() * 128);
@@ -17,36 +19,47 @@ const getRandomColor = (): string => {
   return `rgb(${red}, ${green}, ${blue})`;
 };
 
-const transition = "all 2s";
+const transition = "all 1s";
 
 function App() {
   const [quote, setQuote] = useState<Quote>(getRandomQuote());
   const [randomColor, setRandomColors] = useState<string>(getRandomColor());
+  const [fade, setFade] = useState(true); // Estado para controlar la opacidad
 
   useEffect(() => {
     fetch(
       "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
     )
-      .then((resp) => {
-        return resp.json();
-      })
+      .then((resp) => resp.json())
       .then((data) => {
-        setQuote(getRandomQuote());
-        console.log(data);
+        setQuote(data.quotes[Math.floor(Math.random() * data.quotes.length)]);
       });
   }, []);
 
   const changeQuote = () => {
-    setQuote(getRandomQuote());
-    setRandomColors(getRandomColor());
+    setFade(false); // Ocultar el texto
+
+    setTimeout(() => {
+      setQuote(getRandomQuote());
+      setRandomColors(getRandomColor());
+      setFade(true); // Mostrar el texto después del cambio
+    }, 500); // Esperar antes de cambiar la cita
   };
+
   return (
     <div
       className="background"
       style={{ backgroundColor: randomColor, transition }}
     >
       <div id="quote-box">
-        <div className="quote-content" style={{ color: randomColor }}>
+        <div
+          className="quote-content"
+          style={{
+            color: randomColor,
+            transition: "opacity 0.5s ease", // Transición de opacidad
+            opacity: fade ? 1 : 0, // Cambia la opacidad basado en el estado
+          }}
+        >
           <h2 id="text">
             <FaQuoteLeft size="30" style={{ marginLeft: "10px" }} />
             {quote.quote}
